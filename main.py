544a8198bd15b1a16061c5230ead3808d1a42103
converto <convertoagency@gmail.com>
2023-01-06 22:25:53 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from config.db import *
-from routes.blogs import blogs
+from routes.blog import blogs
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(blogs)
+
+origins = [ "http://localhost:4200" ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
